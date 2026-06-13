16481ca5d5fd9d5056ebcaf1b24233273aa9040f
Santi-sv <santiagovarela29011999@gmail.com>
2026-06-13 00:23:40 -0300

    Conectar Supabase publico

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.SPOTRA_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: 'https://threviqdxzbjsdxbjubm.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_qKSfT5KdVmvusD2BAFvlPg_k1-Dc9-a',
   GOOGLE_MAPS_API_KEY: '',
   GOOGLE_MAP_ID: '',
   DEFAULT_COUNTRY: 'UY',
   DEFAULT_CENTER: { lat: -34.9011, lng: -56.1645 },
   DEFAULT_ZOOM: 12,
-  MOCK_MODE: true
+  MOCK_MODE: false
 };
