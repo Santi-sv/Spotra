9bb24fe9fb5c5bb50b529e490c179c0412d9cd3f
Santi-sv <santiagovarela29011999@gmail.com>
2026-06-13 23:46:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SPOTRA_CONFIG = {
   SUPABASE_URL: 'https://threviqdxzbjsdxbjubm.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_qKSfT5KdVmvusD2BAFvlPg_k1-Dc9-a',
-  GOOGLE_MAPS_API_KEY: '',
+  GOOGLE_MAPS_API_KEY: 'AIzaSyBwqwSAzbWWY07lyV71bp6XfDSOj4qZmT0',
   GOOGLE_MAP_ID: '',
   DEFAULT_COUNTRY: 'UY',
   DEFAULT_CENTER: { lat: -34.9011, lng: -56.1645 },
